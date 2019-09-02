7f1035f156ba389040a3ace6d9b00962e54f044e
Gabriel Pereira da Silva <gabriel.gps@outlook.com>
2019-09-02 01:42:20 -0300

    Base textarea component

---
+++ src/modules/core/components/base/AppTextarea.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="textarea-wrapper">
+    <label
+      v-if="label"
+      class="label"
+    >
+      {{ label }}
+    </label>
+    <textarea
+      class="textarea"
+      v-bind="$attrs"
+      @input="$emit('update', $event.target.value)"
+      v-on="$listeners"
+    ></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  model: {
+    event: 'update',
+  },
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'Styles/design/index.scss';
+
+.textarea-wrapper {
+  .label {
+    text-transform: uppercase;
+  }
+  .textarea {
+    display: block;
+    width: 100%;
+    padding: $size-input-padding-vertical $size-input-padding-horizontal;
+    margin-bottom: $size-grid-padding;
+    line-height: 1;
+    border: $size-input-border solid $color-input-border;
+    border-radius: $size-input-border-radius;
+  }
+}
+</style>
